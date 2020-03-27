dd13aa5849607755a2637313dfba52a7089f2564
CharlieBrownCharacter <bruno11.francisco@gmail.com>
2020-03-27 19:20:38 +0100

    - added option to camelCase final object

---
+++ src/transfromKeys/index.js
@@ -0,0 +1,9 @@
+import { toCamel } from "./toCamel";
+
+export const transformKeys = (object, options) => {
+	if (options.transformKeys === "camelCase") {
+		return toCamel(object);
+	}
+	
+	return object;
+};
